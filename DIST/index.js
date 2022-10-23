3d0133feac9a442efbb3ee8b9816297c31353143
HP ELITEBOOK <aishat.omojole@gmail.com>
2022-10-23 22:17:56 +0100

    Basic HTML,CSS and JS completed

--- DIST/index.js
+++ DIST/index.js
@@ -1,11 +1,14 @@
 const navIcon=document.querySelector('.fa-bars');
 const toggleIcon=document.querySelector('.toggle-icon');
+const cancelled=document.querySelector('.fa-sharp');
 let clicked=true;
 const toggleNavbar=function(){
     if(clicked){
 
         document.querySelector('.nav-lists').classList.add('dropdown');
-    
+    document.querySelector('.fa-sharp').classList.remove('x-mark');
+    document.querySelector('.fa-bars').classList.add('bars');
+
         clicked=false;
     }
     else{
@@ -15,4 +18,14 @@ const toggleNavbar=function(){
     clicked=true;
     }
 };
-navIcon.addEventListener('click',toggleNavbar)
+navIcon.addEventListener('click',toggleNavbar);
+const cancel=function(){
+    if(!clicked){
+        document.querySelector('.nav-lists').classList.remove('dropdown');
+
+        document.querySelector('.fa-bars').classList.remove('bars');
+        document.querySelector('.fa-sharp').classList.add('x-mark');
+
+    }
+}
+cancelled.addEventListener('click',cancel);
